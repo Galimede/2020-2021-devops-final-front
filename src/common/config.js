4b6a562c780cc103d25c29d7d339437fba970fe6
Galimede <mat.degand@outlook.fr>
2021-01-10 14:21:27 +0100

    :wrench: Change url back with "/api"

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.PROD_BACK_URL || "https://tp-devops-final.herokuapp.com/"
+    ? process.env.PROD_BACK_URL || "https://tp-devops-final.herokuapp.com/api"
     : "http://localhost:2020/api";
 export default API_URL;
